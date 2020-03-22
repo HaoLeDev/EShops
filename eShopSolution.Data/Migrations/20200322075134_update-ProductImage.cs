7294323fcbc838ca0d6a5dc4a959bc2b36859562
Lê Vĩnh Hảo <vinhhao2604@gmail.com>
2020-03-23 06:35:58 +0700

    Test Api

---
+++ eShopSolution.Data/Migrations/20200322075134_update-ProductImage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class updateProductImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagePath",
+                table: "ProductImages",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Caption",
+                table: "ProductImages",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 3, 22, 14, 51, 33, 415, DateTimeKind.Local).AddTicks(6220));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagePath",
+                table: "ProductImages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Caption",
+                table: "ProductImages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 3, 22, 12, 5, 40, 62, DateTimeKind.Local).AddTicks(1809));
+        }
+    }
+}
