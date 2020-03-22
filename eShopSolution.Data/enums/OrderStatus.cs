4396e3ea4a063c665929523cd27a48c6b805a1b0
Lê Vĩnh Hảo <vinhhao2604@gmail.com>
2020-03-22 10:03:22 +0700

    Add migrations

---
+++ eShopSolution.Data/enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.enums
+{
+    public enum OrderStatus
+    {
+        InProgress,
+        Confirmed,
+        Shipping,
+        Success,
+        Canceled
+    }
+}
